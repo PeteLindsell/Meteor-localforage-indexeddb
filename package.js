2d5b2e49b3f01dac7bfa436646a93efb630b6b83
Morten Henriksen <mh@gi2.dk>
2015-08-25 02:17:28 +0200

    1.2.4-rc.1 Meteor 1.2-rc.4 test

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   name: 'raix:localforage-indexeddb',
-  version: '1.2.4',
+  version: '1.2.4-rc.1',
   summary: "Offline storage driver IndexedDB"
 });
 
 Package.on_use(function (api) {
-  // api.versionsFrom('1.2');
+  api.versionsFrom('METEOR@1.2-rc.4');
   api.use('ecmascript', 'client');
 
   api.addFiles('indexeddb.js', 'client');
